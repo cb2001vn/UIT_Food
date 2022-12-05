24a8df69a30629192f590936276266ac13251a5d
UIT-19521243 <19521243@gm.uit.edu.vn>
2022-12-06 04:14:40 +0700

    Edit HomePage

--- UIT_Food/UIT_Food/Views/HomePage.xaml.cs
+++ UIT_Food/UIT_Food/Views/HomePage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -12,10 +14,29 @@ namespace UIT_Food.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
+        List<Restaurant> restaurants;
         public HomePage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            InitRestaurant();
+        }
+
+        async void InitRestaurant()
+        {
+            restaurants = new List<Restaurant>();
+            HttpClient httpClient = new HttpClient();
+            var ResList = await httpClient.GetStringAsync("http://192.168.1.148/webapi/api/RestaurantController/GetNhaHang");
+            var RestListCV = JsonConvert.DeserializeObject<List<Restaurant>>(ResList);
+            restaurants = RestListCV;
+ 
+
+            Name1.Text = restaurants[5].TEN;
+            Name2.Text = restaurants[6].TEN;
+            Name3.Text = restaurants[7].TEN;
+            Name4.Text = restaurants[8].TEN;
+            Name5.Text = restaurants[9].TEN;
+
         }
 
         private async void GetLocation_Tapped(object sender, EventArgs e)
